9564a5e547bc8ff64eae5f0c0f1c719b9fa9dcde
William <lliw1236@yahoo.com>
2012-04-15 22:55:12 -0700

    8.5 q2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,79 @@
 module SessionsHelper
+
+# tweaked more from bottom
+  def sign_in(user)
+    cookies.permanent.signed[:remember_token] = [user.id]
+    session[:user_id] = user.id
+    self.current_user = user
+  end
+
+  def sign_out
+    cookies.delete(:remember_token)
+    session[:user_id] = nil
+    self.current_user = nil
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= user_from_remember_token
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  private
+
+  def user_from_remember_token
+      remember_token = cookies[:remember_token]
+      User.find_by_remember_token(remember_token) unless remember_token.nil?
+  end
+
+  def remember_token
+      cookies.signed[:remember_token] || [nil, nil]
+  end
+
+=begin
+#slight tweak from original
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
+    session[:user_id] = user.id
+    current_user = user
+  end
+
+  def sign_out
+    cookies.delete(:remember_token)
+    session[:user_id] = nil
+    current_user = nil
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= user_from_remember_token
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  private
+   def user_from_remember_token
+    remember_token = cookies[:remember_token]
+    User.find_by_remember_token(remember_token) unless remember_token.nil?
+  end
+=end
+
+=begin
+#before the exercise
+  def sign_in(user)
+    cookies.permanent.[:remember_token] = user.remember_token
     current_user = user
   end
 
@@ -27,5 +100,5 @@ module SessionsHelper
     remember_token = cookies[:remember_token]
     User.find_by_remember_token(remember_token) unless remember_token.nil?
   end
-
+=end
 end
