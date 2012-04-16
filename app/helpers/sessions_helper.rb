46cc63d6c0ce2e6edb56c2f386e9d0bfa00b61a9
William <lliw1236@yahoo.com>
2012-04-15 23:02:49 -0700

    8.5 q2 self removal

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,13 +4,13 @@ module SessionsHelper
   def sign_in(user)
     cookies.permanent.signed[:remember_token] = [user.id]
     session[:user_id] = user.id
-    self.current_user = user
+    current_user = user
   end
 
   def sign_out
     cookies.delete(:remember_token)
     session[:user_id] = nil
-    self.current_user = nil
+    current_user = nil
   end
 
   def current_user=(user)
