20e307d13190b9dddf767f9a598b7dce63bf14a3
William <lliw1236@yahoo.com>
2012-04-17 23:04:35 -0700

    paginated users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_filter :correct_user, only: [:edit, :update]
 
   def index
-    @user = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
