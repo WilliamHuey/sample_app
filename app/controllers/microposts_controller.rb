c9bc41e5fa7d2b1edacc573ee07f1d3b3ce6718b
William <lliw1236@yahoo.com>
2012-04-21 16:55:15 -0700

    2 errors micropost, missing templates

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,8 +1,18 @@
 class MicropostsController < ApplicationController
   before_filter :signed_in_user, only: [:create, :destroy]
 
-  def create
+  def index
 
+  end
+
+  def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_path
+    else
+      render 'static/pages/home'
+    end
   end
 
   def destroy
