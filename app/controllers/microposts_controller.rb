bc79702de358a68f5eb90a08e6e823cc470a050c
William <lliw1236@yahoo.com>
2012-04-21 17:13:47 -0700

    creating microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -11,7 +11,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_path
     else
-      render 'static/pages/home'
+      render 'static_pages/home'
     end
   end
 
