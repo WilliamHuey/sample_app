3a7d7bb7a521563288ec3e461b676db235a5534b
William <lliw1236@yahoo.com>
2012-04-26 12:04:00 -0700

    a working follow and unfollow button the standard way

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -99,7 +99,18 @@ end
           end
           specify { response.should redirect_to(signin_path) }
         end
+      end
 
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path}
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1)}
+          specify { response.should redirect_to(signin_path) }
+        end
       end
 
       describe "when attempting to visit a protected page" do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -68,12 +68,61 @@ describe "UserPages" do
     it { should have_selector('h1', text: user.name) }
     it { should have_selector('title', text: user.name) }
 
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
 
-  describe "microposts" do
-    it { should have_content(m1.content) }
-    it { should have_content(m2.content) }
-    it { should have_content(user.microposts.count) }
-  end
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      describe "following a user" do
+        before { visit user_path(other_user) }
+
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
+
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          it { should have_selector('input', value: "Unfollow") }
+        end
+      end
+
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
+
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
+
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Unfollow" }
+          it { should have_selector('input', value: 'Follow') }
+        end
+      end
+    end
   end
 
   describe "signup path" do
@@ -169,7 +218,7 @@ describe "UserPages" do
 
   describe "following/followers" do
     let(:user) { FactoryGirl.create(:user) }
-    let(:other_user) {FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
     before { user.follow!(other_user) }
 
     describe "followed users" do
@@ -190,7 +239,7 @@ describe "UserPages" do
       end
 
       it { should have_selector('title', text: full_title('Followers')) }
-      it { should have_selector('h3', text: 'Followers')}
+      it { should have_selector('h3', text: 'Followers') }
       it { should have_link(user.name, href: user_path(user)) }
     end
   end
